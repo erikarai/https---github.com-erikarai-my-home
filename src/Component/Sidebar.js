09afc726ba06386d52271caf3af4195f99b361e1
Erika Rai <erikarai0015@gmail.com>
2023-10-02 10:34:41 +0530

    hi, some changes on sidebar

--- src/Component/Sidebar.js
+++ src/Component/Sidebar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Sidebar() {
   return (
      <div className='d-flex'>
-    <div className="p-2 ">
+    <div className="p-2 style={{ flex : 1.5}}">
     <div className="row flex-nowrap">
         <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-light">
             <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
@@ -16,7 +16,7 @@ function Sidebar() {
                             <i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">About</span> </a>
                         <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
                             <li className="w-100">
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Overview</span></a>
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline" justifyContet='center' alignItems= 'center'>Overview</span></a>
                             </li>
                             <li>
                                 <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Prizes</span></a>
@@ -25,45 +25,21 @@ function Sidebar() {
                     </li>
                     <li>
                         <a href="#" className="nav-link px-0 align-middle">
-                            <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Orders</span></a>
+                            <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Rules</span></a>
                     </li>
                     <li>
                         <a href="#" className="nav-link px-0 align-middle">
-                            <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Orders</span></a>
+                            <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Judges</span></a>
                     </li>
                     <li>
                         <a href="#submenu2" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
-                            <i className="fs-4 bi-bootstrap"></i> <span className="ms-1 d-none d-sm-inline">Bootstrap</span></a>
-                        <ul className="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                            <li className="w-100">
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1</a>
-                            </li>
-                            <li>
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2</a>
-                            </li>
-                        </ul>
+                            <i className="fs-4 bi-bootstrap"></i> <span className="ms-1 d-none d-sm-inline">Submission Guideline</span></a>
+    
                     </li>
-                    <li>
-                        <a href="#submenu3" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                            <i className="fs-4 bi-grid"></i> <span className="ms-1 d-none d-sm-inline">Products</span> </a>
-                            <ul className="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                            <li className="w-100">
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 1</a>
-                            </li>
-                            <li>
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 2</a>
-                            </li>
-                            <li>
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 3</a>
-                            </li>
-                            <li>
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 4</a>
-                            </li>
-                        </ul>
-                    </li>
+                
                     <li>
                         <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
+                            <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">About Incedo</span> </a>
                     </li>
                 </ul>
                 <hr/>
@@ -72,10 +48,10 @@ function Sidebar() {
         </div>
         </div>
 </div>
-            <div className='p-2' style={{background : 'black'}}>
+            <div className='p-2' style={{background : 'black', flex : 3}}>
              content area
            </div>
-           <div className='p-2'>
+           <div className='p-2' style={{flex : 0.5}}>
              hello erika
            </div>
 </div>
