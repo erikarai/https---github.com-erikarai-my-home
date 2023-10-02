c8f0eb78a8bd7d5bc2be048c8a7508ee3ba75eac
Erika Rai <erikarai0015@gmail.com>
2023-10-02 23:17:05 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Navbar from './Component/Navbar';
 import Sidebar from './Component/Sidebar';
 import MainBody from './Component/MainBody'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Reasons from './Component/Reasons'
 
 function App() {
   return (<>
   <Navbar/>
+  <Reasons />
   </>
   );
 }
